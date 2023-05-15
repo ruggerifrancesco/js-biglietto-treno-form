277d247ffa734bb995dd0db969244ab86b11f5d1
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-15 17:35:30 +0200

    MAJOR HOTFIX for discount calculator and messages

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,17 @@ console.log("JS Ticket Form - Loaded");
     let distanceValue = document.getElementById("input-distance");
     let ageValue = document.getElementById("input-age");
 
-    // Tickets
-    let priceTicket = distanceValue.value * 0.233;
-    let getDiscountMessage;
-    let getOutputMessage;
-
 
 // Fixed Variables
 const calculatorSubmit = document.getElementById("inputs-submit");
 
+// Tickets
 calculatorSubmit.addEventListener("click",
     function () {
+        let priceTicket = distanceValue.value * 0.233;
+        let discountPrice = 0;
+        let discountMessage;
+
         //Console message for making sure the inputs
         //from the user are get received correctly!
         console.log(distanceValue.value + " " + ageValue.value + " " + 
@@ -23,24 +23,28 @@ calculatorSubmit.addEventListener("click",
 
         // Ticket Age Cycle
         if (ageValue.value <= 0 || ageValue.value > 110) {
-            getDiscountMessage = "Dati non corretti!";
+            discountMessage = "Dati non corretti!";
         } else if (ageValue.value < 18) {
-            getDiscountMessage = "Prezzo Scontato per minori 18!";
-            priceTicket = priceTicket * (19.4 / 100);
+
+            discountPrice = priceTicket * (19.4 / 100);
+            priceTicket = priceTicket - discountPrice;
+            discountMessage = "Prezzo Scontato per minori 18!";
+
         } else if (ageValue.value >= 65) {
-            getDiscountMessage = "Prezzo Scontato per over 65!";
-            priceTicket = priceTicket * (37.7 / 100);
+
+            discountPrice = priceTicket * (37.7 / 100);
+            priceTicket = priceTicket - discountPrice;
+            discountMessage = "Prezzo Scontato per over 65!";
+
         } else {
-            
+            discountMessage = "";
         }
-
-        alert(priceTicket);
-
+        
         // Return State
             // Console Log
             console.log("Prezzo biglietto:" + priceTicket.toFixed(2));
             // DOM Element
             document.getElementById("calc-ticket-output").innerHTML += 
-                priceTicket.toFixed(2) + "&euro;" + getDiscountMessage;
+                priceTicket.toFixed(2) + "&euro;" + discountMessage;
     }
 )
