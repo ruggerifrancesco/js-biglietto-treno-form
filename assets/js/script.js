a986f1e1c39069544c4df388af7a8af0ed2c1b57
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-15 15:36:23 +0200

    Added a button selector by id and eventlistener

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+console.log("JS Ticket Form - Loaded");
+
+let distance = document.getElementById("input-distance");
+let age = document.getElementById("input-age");
+
+const calculatorSubmit = document.getElementById("inputs-submit");
+
+calculatorSubmit.addEventListener("click",
+    function () {
+        
+    }
+)
+
+console.log(distance.value + age.value + "Inputs Taken Correctly");
